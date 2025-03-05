4f283c8ffe08a3d1d2426e3c7153a15a0541df1d
kayro-is <luffykosuki@gmail.com>
2025-03-05 22:31:48 +0100

    designe du composant Footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,12 +1,98 @@
-import React from 'react';
-import './Footer.scss';
+import React from "react";
+import "./Footer.scss";
 
-const Footer = () => {
-    return (
-        <div className='footer'>
-            <p>Pro-Task &copy; 2021</p>
+function Footer() {
+  return (
+    <div className="footer">
+      <div className="container">
+        <div className="top">
+          <div className="item">
+            <h2>Categories</h2>
+            <span>Graphics & Design</span>
+            <span>Digital Marketing</span>
+            <span>Writing & Translation</span>
+            <span>Video & Animation</span>
+            <span>Music & Audio</span>
+            <span>Programming & Tech</span>
+            <span>Data</span>
+            <span>Business</span>
+            <span>Lifestyle</span>
+            <span>Photography</span>
+            <span>Sitemap</span>
+          </div>
+          <div className="item">
+            <h2>A propos</h2>
+            <span>Press & News</span>
+            <span>Partnerships</span>
+            <span>Privacy Policy</span>
+            <span>Terms of Service</span>
+            <span>Intellectual Property Claims</span>
+            <span>Investor Relations</span>
+            <span>Contact Sales</span>
+          </div>
+          <div className="item">
+            <h2>Support</h2>
+            <span>Help & Support</span>
+            <span>Trust & Safety</span>
+            <span>Selling on Liverr</span>
+            <span>Buying on Liverr</span>
+          </div>
+          <div className="item">
+            <h2>Community</h2>
+            <span>Customer Success Stories</span>
+            <span>Community hub</span>
+            <span>Forum</span>
+            <span>Events</span>
+            <span>Blog</span>
+            <span>Influencers</span>
+            <span>Affiliates</span>
+            <span>Podcast</span>
+            <span>Invite a Friend</span>
+            <span>Become a Seller</span>
+            <span>Community Standards</span>
+          </div>
+          <div className="item">
+            <h2>More From Fiverr</h2>
+            <span>kayss Business</span>
+            <span>kayss Pro</span>
+            <span>kayss Logo Maker</span>
+            <span>kayss Guides</span>
+            <span>Get Inspired</span>
+            <span>kayss Select</span>
+            <span>ClearVoice</span>
+            <span>Kayss Workspace</span>
+            <span>Learn</span>
+            <span>Working Not Working</span>
+          </div>
         </div>
-    );
+        <hr />
+        <div className="bottom">
+          <div className="left">
+            <h2>Kayss</h2>
+            <span>© kayss International Ltd. 2023</span>
+          </div>
+          <div className="right">
+            <div className="social">
+              <img src="/img/x.png" alt="" />
+              <img src="/img/facebook.png" alt="" />
+              <img src="/img/linkedin.png" alt="" />
+              <img src="/img/pinterest.png" alt="" />
+              <img src="/img/instagram.png" alt="" />
+            </div>
+            <div className="link">
+              <img src="/img/language.jpg" alt="" />
+              <span>Francais</span>
+            </div>
+            <div className="link">
+              <img src="/img/coin.png" alt="" />
+              <span>EUR</span>
+            </div>
+            <img src="/img/accessibility.png" alt="" />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Footer;
